0b4bf93e4876433e10a0a6382142dac36046dc6d
pdbartlett <pdbartlett@a7d2392a-17e3-11df-bcdc-a7d16bff891f>
2010-03-11 21:37:11 +0000

    Tidy up trait hierarchy a bit by moving some type parameters to abstract type members

--- trunk/scala/data/Dimension.scala
+++ trunk/scala/data/Dimension.scala
@@ -13,11 +13,3 @@ trait Dimension[T] {
   def node(t: T): Node[T]
 }
 
-trait Dimensionality1[T1] {
-  type K = T1
-}
-
-trait Dimensionality2[T1, T2] {
-  type K = (T1, T2)
-}
-
--- trunk/scala/data/Measure.scala
+++ trunk/scala/data/Measure.scala
@@ -1,13 +1,55 @@
 package pdbartlett.lascala.scala.data
 
-trait Measure[V] {
+trait Measure {
   type K
+  type V
   def aggData: Iterable[(K, V)]
 }
 
-trait PreAggregated[V] extends Measure[V] {
+trait Measure1[U, T1] extends Measure {
+  type V = U
+  type D1 = T1
+  type K = D1
+}
+
+trait Measure2[U, T1, T2] extends Measure {
+  type V = U
+  type D1 = T1
+  type D2 = T2
+  type K = (D1, D2)
+}
+
+trait PreAggregated extends Measure {
   def data: Iterable[(K, V)]
   def aggData = data
+}
+
+trait BulkAggregated2 {
+  type V
+  type D1
+  type D2
+  type P = (D1, D2)
+
+  def data: Iterable[(P, V)]
+  def dim1: Dimension[D1]
+  def dim2: Dimension[D2]
+  def newAccumulator(): Accumulator[V]
+  
+  def aggData = {
+    var map = Map[P, Accumulator[V]]()
+    for {
+      datum <- data
+      leaf1 = dim1.node(datum._1._1)
+      leaf2 = dim2.node(datum._1._2)
+      node1 <- leaf1.selfAndAncestors
+      node2 <- leaf2.selfAndAncestors
+    } {
+      val key = (node1.value, node2.value)
+      val existing = map.getOrElse(key, newAccumulator())
+      map += ((key, existing + datum._2))
+    }
+    map.map((entry: (P, Accumulator[V])) => (entry._1, entry._2.value))
+  }
 }
 
 trait Accumulator[V] {
@@ -22,30 +64,7 @@ trait IntsAggregatedBySum {
   }
 }
 
-trait BulkAggregated2[V, D1, D2] extends Measure[V] with Dimensionality2[D1, D2] {
-  def data: Iterable[(K, V)]
-  def dim1: Dimension[D1]
-  def dim2: Dimension[D2]
-  def newAccumulator(): Accumulator[V]
-  
-  def aggData = {
-    var map = Map[K, Accumulator[V]]()
-    for {
-      datum <- data
-      leaf1 = dim1.node(datum._1._1)
-      leaf2 = dim2.node(datum._1._2)
-      node1 <- leaf1.selfAndAncestors
-      node2 <- leaf2.selfAndAncestors
-    } {
-      val key = (node1.value, node2.value)
-      val existing = map.getOrElse(key, newAccumulator())
-      map += ((key, existing + datum._2))
-    }
-    map.map((entry: (K, Accumulator[V])) => (entry._1, entry._2.value))
-  }
-}
-
-trait Storage[V] extends Measure[V] {
+trait Storage extends Measure {
   def store(aggregated: Iterable[(K, V)]): Unit
   def retrieve(): Iterable[(K, V)]
   private var stored = false
@@ -58,13 +77,13 @@ trait Storage[V] extends Measure[V] {
   }
 }
 
-trait InMemoryStorage[V] extends Storage[V] {
+trait InMemoryStorage extends Storage {
   var map: Map[K, V] = Map()
   def store(aggregated: Iterable[(K, V)]) { map ++= aggregated }
   def retrieve() = map
 }
 
-trait FactBasedMeasure2[F, V, D1, D2] extends Measure[V] with Dimensionality2[D1, D2] {
+trait FactBasedMeasure2[F, V, D1, D2] extends Measure2[V, D1, D2] {
   def factData: Iterable[F]
   def extract(f: F): (K, V)
   def data = factData map extract
