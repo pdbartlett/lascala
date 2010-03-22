a48dc68d5ff03431d5b8670d8858c7695a0db173
pdbartlett <pdbartlett@a7d2392a-17e3-11df-bcdc-a7d16bff891f>
2010-03-22 17:12:55 +0000

    Add beginnings of query functionality

--- trunk/scala/data/Dimension.scala
+++ trunk/scala/data/Dimension.scala
@@ -3,13 +3,39 @@ package pdbartlett.lascala.scala.data
 abstract class Node[T] {
   def value: T
   def parent: Option[Node[T]]
+  def isLeaf: Boolean
   def selfAndAncestors: List[Node[T]] = parent match {
     case None => List(this)
     case Some(node) => this :: node.selfAndAncestors
   }
 }
 
+abstract class NodeSpec[T] {
+  def nodes: Iterable[T]
+  def matches(t: T): Boolean
+}
+
+class NodeSet[T](ts: Seq[T]) extends NodeSpec[T] {
+  def nodes = ts
+  def matches(t: T) = ts.contains(t)
+}
+
+object NodeSet {
+  def apply[T](ts: T*) = new NodeSet[T](ts)
+}
+
+class PredicateNodes[T](dim: Dimension[T], pred: T => Boolean) extends NodeSpec[T] {
+  def nodes = dim.values
+  def matches(t: T) = pred(t)
+}
+
+object PredicateNodes {
+  def apply[T](dim: Dimension[T], pred: T => Boolean) = new PredicateNodes(dim, pred)
+}
+
 trait Dimension[T] {
   def node(t: T): Node[T]
+  def defaultValue: T
+  def values: Iterable[T]
 }
 
--- trunk/scalatests/data/TestData.scala
+++ trunk/scalatests/data/TestData.scala
@@ -51,25 +51,26 @@ trait DataTestUtils extends TestData {
 
 object Colour extends Enumeration { val Red, Green, Blue = Value }
 
-class SimpleNode[T](val value: T, val parent: Option[Node[T]]) extends Node[T]
+class SimpleNode[T](val value: T, val parent: Option[Node[T]], val isLeaf: Boolean) extends Node[T]
 
 object ColoursDim extends Dimension[Colour.Value] {
-  private val rootNode = new SimpleNode[Colour.Value](null, None)
-  private val redNode = new SimpleNode[Colour.Value](Colour.Red, Some(rootNode))
-  private val greenNode = new SimpleNode[Colour.Value](Colour.Green, Some(rootNode))
-  private val blueNode = new SimpleNode[Colour.Value](Colour.Blue, Some(rootNode))
-  private val nodes = Map(
-      Colour.Red -> redNode,
-      Colour.Green -> greenNode,
-      Colour.Blue -> blueNode)
-  def node(colour: Colour.Value) = if (colour == null) rootNode else nodes(colour)
+  private val rootNode = new SimpleNode[Colour.Value](null, None, false)
+  private val redNode = new SimpleNode[Colour.Value](Colour.Red, Some(rootNode), true)
+  private val greenNode = new SimpleNode[Colour.Value](Colour.Green, Some(rootNode), true)
+  private val blueNode = new SimpleNode[Colour.Value](Colour.Blue, Some(rootNode), true)
+  private val nodes = Map(Colour.Red -> redNode, Colour.Green -> greenNode, Colour.Blue -> blueNode)
+  val defaultValue = null
+  val values = nodes.keySet ++ List(defaultValue)
+  def node(colour: Colour.Value) = if (colour == defaultValue) rootNode else nodes(colour)
 }
 
 object CountriesDim extends Dimension[String] {
-  private val rootNode = new SimpleNode("WW", None)
-  private val ukNode = new SimpleNode("UK", Some(rootNode))
-  private val usNode = new SimpleNode("US", Some(rootNode))
+  private val rootNode = new SimpleNode("WW", None, false)
+  private val ukNode = new SimpleNode("UK", Some(rootNode), true)
+  private val usNode = new SimpleNode("US", Some(rootNode), true)
   private val nodes = Map("WW" -> rootNode, "UK" -> ukNode, "US" -> usNode)
+  val defaultValue = "WW"
+  val values = nodes.keySet
   def node(country: String) = nodes(country)
 }
 
@@ -89,13 +90,18 @@ class TestFact(val country: String, val colour: Colour.Value, val count: Int)
 
 object TestFact {
   def apply(country: String, colour: Colour.Value, count: Int) = new TestFact(country, colour, count)
-  def unapply(f: TestFact) = Some((f.country, f.colour, f.count)) // Extractors seem a natural fit for fact-based measures, but aren't yet
+  // Extractors seem as if they'd be a natural fit for fact-based measures, but aren't yet of any use yet.
+  def unapply(f: TestFact) = Some((f.country, f.colour, f.count))
 }
 
-object TestPreAggMeasure1 extends TestBulkDataSource with Measure1[Int, String] with PreAggregated
+object TestPreAggMeasure1 extends TestBulkDataSource with Measure1[Int, String] with PreAggregated {
+  val dim1 = null // So cannot be aggregared or queried.
+}
 
 object TestPreAggMeasure2 extends Measure2[Int, String, Colour.Value] with PreAggregated with TestData {
   val data = twoDimAggData
+  val dim1 = CountriesDim
+  val dim2 = ColoursDim
 }
 
 object TestBulkAggMeasure extends Measure2[Int, String, Colour.Value] with BulkAggregated2 with IntsAggregatedBySum with TestData {
@@ -106,10 +112,14 @@ object TestBulkAggMeasure extends Measure2[Int, String, Colour.Value] with BulkA
 
 object TestReadOnceMeasure extends Measure2[Int, String, Colour.Value] with PreAggregated {
   val data = new ReadOnceData
+  val dim1 = CountriesDim
+  val dim2 = ColoursDim
 }
 
-object TestStorageMeasure extends Measure2[Int, String, Colour.Value] with PreAggregated with InMemoryStorage {
+object TestStorageMeasure extends LookupMeasure2[Int, String, Colour.Value] with PreAggregated with InMemoryStorage {
   val data = new ReadOnceData
+  val dim1 = CountriesDim
+  val dim2 = ColoursDim
 }
 
 object TestFactBasedMeasure extends FactBasedMeasure2[TestFact, Int, String, Colour.Value] with BulkAggregated2 with IntsAggregatedBySum {
