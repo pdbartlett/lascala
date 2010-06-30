9b7d3ac02392a2f6e76ac4bb833cdf55ad9117e1
pdbartlett <pdbartlett@a7d2392a-17e3-11df-bcdc-a7d16bff891f>
2010-06-30 13:50:25 +0000

    Get SBT compile/test working with 2.7.7 and 2.8.0.RC6.

--- trunk/project/build/LascalaProject.scala
+++ trunk/project/build/LascalaProject.scala
@@ -5,7 +5,11 @@ class LascalaProject(info: ProjectInfo) extends ParentProject(info) {
 	val lib = project("lib", "lib", new LibProject(_))
 
 	class LibProject(info: ProjectInfo) extends DefaultProject(info) {
-		
-		val scalatest = "org.scalatest" % "scalatest" % "1.0" % "test"
+		val scalaToolsSnapshots = ScalaToolsSnapshots
+		val stVersion = buildScalaVersion match {
+			case "2.8.0.RC6" => "1.2-for-scala-2.8.0.RC6-SNAPSHOT"
+			case _ => "1.0"
+		}
+		val scalatest = "org.scalatest" % "scalatest" % stVersion % "test"
 	}
 }
