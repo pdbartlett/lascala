3dea08a43be2ac4cbafc6d8c8174d0450400c2d6
pdbartlett <pdbartlett@a7d2392a-17e3-11df-bcdc-a7d16bff891f>
2010-07-02 13:46:31 +0000

    Get demo web project up-and-running with Jetty, plus other minor corrections.

--- trunk/project/build/LascalaProject.scala
+++ trunk/project/build/LascalaProject.scala
@@ -3,6 +3,7 @@ import sbt._
 class LascalaProject(info: ProjectInfo) extends ParentProject(info) {
 
 	val lib = project("lib", "lib", new LibProject(_))
+	val web = project("web", "web", new WebProject(_))
 
 	class LibProject(info: ProjectInfo) extends DefaultProject(info) {
 		val scalaToolsSnapshots = ScalaToolsSnapshots
@@ -11,5 +12,14 @@ class LascalaProject(info: ProjectInfo) extends ParentProject(info) {
 			case _ => "1.0"
 		}
 		val scalatest = "org.scalatest" % "scalatest" % stVersion % "test"
+	}
+	
+	class WebProject(info: ProjectInfo) extends DefaultWebProject(info) {
+		// Depend on servlet API rather than any particular container at compile time.
+		val servletApi = "javax.servlet" % "servlet-api" % "2.5"
+		
+		// Use Jetty for testing (SBT issue with v7, so use v6 for now).
+		val jetty6 = "org.mortbay.jetty" % "jetty" % "6.1.24" % "test"
+		//val jetty7 = "org.eclipse.jetty" % "jetty-webapp" % "7.1.4.v20100610" % "test"
 	}
 }
