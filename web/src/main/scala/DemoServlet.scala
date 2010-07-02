11e11cedd01001ff3c3b07be534bb8b8ff5fd281
pdbartlett <pdbartlett@a7d2392a-17e3-11df-bcdc-a7d16bff891f>
2010-07-02 14:58:24 +0000

    Minor changes to DemoServlet class

--- web/src/main/scala/DemoServlet.scala
+++ web/src/main/scala/DemoServlet.scala
@@ -5,8 +5,12 @@ import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 class DemoServlet extends HttpServlet {
-  override def doGet(req: HttpServletRequest, resp: HttpServletResponse) {
-	val content = <html><body><h1>Demo Servlet</h1><p>Just testing...</p></body></html>
-	resp.getWriter().write(content.toString())
+	override def doGet(req: HttpServletRequest, resp: HttpServletResponse) {
+		val content =
+			<html>
+				<head><title>Demo Servlet</title></head>
+				<body><h1>Demo Servlet</h1><p>Just testing...</p></body>
+			</html>
+	resp.getWriter.write(content.toString)
   }
 }
