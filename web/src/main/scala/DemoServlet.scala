db329a2019a1579beb5978a2a962f95ff566b15c
pdbartlett <pdbartlett@a7d2392a-17e3-11df-bcdc-a7d16bff891f>
2010-07-02 13:46:31 +0000

    Get demo web project up-and-running with Jetty, plus other minor corrections.

---
+++ web/src/main/scala/DemoServlet.scala
@@ -0,0 +1,12 @@
+package pdbartlett.lascala.web
+
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class DemoServlet extends HttpServlet {
+  override def doGet(req: HttpServletRequest, resp: HttpServletResponse) {
+	val content = <html><body><h1>Demo Servlet</h1><p>Just testing...</p></body></html>
+	resp.getWriter().write(content.toString())
+  }
+}
